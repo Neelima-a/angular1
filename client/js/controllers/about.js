435aa39f9cc8eda0ecfa7d2f7d671bf341c17d13
Neelima-a <aneelimaneelu@gmail.com>
2017-09-18 19:18:32 +0530

    used

--- client/js/controllers/about.js
+++ client/js/controllers/about.js
@@ -1,6 +1,14 @@
-app.controller('aboutCtrl',['$scope', function($scope){
+app.controller('aboutCtrl',['$scope','$http', function($scope,$http){
             $scope.abtFunc = function(){
                 alert("this is about page");
+                $http({
+                    method:'GET',
+                    url:'/api/allparallel'
+                }).then(function mySuccess(response) {
+                    console.log(response);
+                },
+                    function myError(response){
+                    console.log(response);
+                });
             }
-
 }]);
--- client/js/controllers/home.js
+++ client/js/controllers/home.js
@@ -1,5 +1,13 @@
-app.controller("homeCtrl",['$scope', function($scope){
+app.controller("homeCtrl",['$scope','$http', function($scope,$http){
     $scope.homeFunc = function(){
         alert("this is homepage");
+        $http({
+            method:'GET',
+            url:'/api/allseries'
+        }).then(function mySuccess(response) {
+            console.log(response);
+        },function myError(response){
+            console.log(response);
+        });
     }
 }]);
